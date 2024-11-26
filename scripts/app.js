84d9fe365ac06e4fb0d10ec62b767a79bbee73b2
BrockSpacek <brockspacek1@gmail.com>
2024-11-26 11:27:24 -0800

    Created a functioning random name generator with a button click

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,37 @@
-let firstName = document.getElementById('firstName');
-let lastName = document.getElementById('lastName');
-let summonPirate = document.getElementById('summonPirate');
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let summonPirate = document.getElementById("summonPirate");
+
+
+
+
+
+function getPiratesData() {
+  return fetch("../data/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      return data.pirates;
+    });
+}
+
+
+getPiratesData();
+
+
+function getRandomPirate(pirates){
+    let randomIndex = Math.floor(Math.random() * pirates.length);
+    console.log([randomIndex]);
+    return pirates[randomIndex];
+}
+
+summonPirate.addEventListener('click', () => {
+    getPiratesData().then( pirates => {
+        let randomPirate = getRandomPirate(pirates);
+        console.log(randomPirate);
+
+        firstName.innerText = randomPirate.firstName
+        lastName.innerText = randomPirate.lastName
+        
+    })
+});
